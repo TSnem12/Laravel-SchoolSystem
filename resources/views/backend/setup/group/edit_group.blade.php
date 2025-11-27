2d29a4acde6a3ad33fa2324b8882e3649ecd4f3d
TSnem12 <tasneemm967@gmail.com>
2025-11-27 23:32:21 +0200

    student group management

--- resources/views/backend/setup/group/edit_group.blade.php
+++ resources/views/backend/setup/group/edit_group.blade.php
@@ -12,7 +12,7 @@
 		 <!-- Basic Forms -->
 		  <div class="box">
 			<div class="box-header with-border">
-			  <h4 class="box-title">Edit Student Year</h4>
+			  <h4 class="box-title">Edit Student Group</h4>
 			  
 			</div>
 			<!-- /.box-header -->
@@ -20,19 +20,21 @@
 			  <div class="row">
 				<div class="col">
 
-	 <form method="post" action="{{ route('update.student.year',$editData->id) }}">
+ <form method="post" action="{{ route('update.student.group',$editData->id) }}">
 	 	@csrf
-		<div class="row">
-		<div class="col-12">	
+					  <div class="row">
+						<div class="col-12">	
+ 
+
  
 
 		<div class="form-group">
-		<h5>Student Year Name <span class="text-danger">*</span></h5>
+		<h5>Student Group Name <span class="text-danger">*</span></h5>
 		<div class="controls">
-			<input type="text" name="name" class="form-control" value="{{ $editData->name }}" > 
-			@error('name')
-			<span class="text-danger">{{ $message }}</span>
-			@enderror
+	 <input type="text" name="name" class="form-control" value="{{ $editData->name }}" > 
+	 @error('name')
+	 <span class="text-danger">{{ $message }}</span>
+	 @enderror
 	  </div>
 		 
 	</div>
@@ -62,6 +64,8 @@
 	  
 	  </div>
   </div>
+
+
 
 
 
