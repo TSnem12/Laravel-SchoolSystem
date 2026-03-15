b7a43111c5b944a48382cefd1fcbeba97970f189
TSnem12 <tasneemm967@gmail.com>
2026-03-15 22:23:40 +0200

    generate students-IDCard

--- routes/web.php
+++ routes/web.php
@@ -309,8 +309,9 @@ Route::group(['middleware' => 'auth'], function() {
         Route::get('student/result/view', [ResultReportController::class, 'ResultView'])->name('student.result.view');
         Route::get('student/result/get', [ResultReportController::class, 'ResultGet'])->name('report.student.result.get');
 
-
-
+        // Student ID Card Routes 
+        Route::get('student/idcard/view', [ResultReportController::class, 'IdcardView'])->name('student.idcard.view');
+        Route::get('student/idcard/get', [ResultReportController::class, 'IdcardGet'])->name('report.student.idcard.get');
 
     }); 
 
